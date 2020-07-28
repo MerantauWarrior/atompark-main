11befcc4753933c73a08672304149f721c648461
Boris <boris1488ss@gmail.com>
2020-07-28 16:03:53 +0300

    documentation api page mobile

--- assets/js/documentation.js
+++ assets/js/documentation.js
@@ -5,22 +5,31 @@ $(document).ready(function () {
     $(this).siblings('.documentation-submenu').slideToggle(250);
   });
 
+  if ($(window).width() < 1025) {
+    $('.documentation-mob-nav').click(function () {
+      $('.documentation-nav').toggleClass('documentation-nav_opened');
+    });
+  }
+
 
   $('pre').each(function (index, pre) {
     var preHtml = document.createElement('div');
     var preLines = document.createElement('div');
     var preCode = document.createElement('div');
+    var preCodeInner = document.createElement('div');
     preHtml.classList.add('preCode');
     preLines.classList.add('preCode-lines');
     preCode.classList.add('preCode-text');
+    preCodeInner.classList.add('preCode-text-inner');
     var arrPre = pre.innerHTML.toString().split(/\r?\n/);
     $(pre).replaceWith(preHtml);
     for (var i = 0; i<arrPre.length; i++){
       if (arrPre[i] && arrPre[i].trim().length !== 0){
         preLines.innerHTML += '<div>'+parseInt(i+1)+'</div>';
-        preCode.innerHTML += arrPre[i].replace(/"([^"]+)"/g, '<span class="preCode-value">"$1"</span>') + '\n';
+        preCodeInner.innerHTML += arrPre[i].replace(/"([^"]+)"/g, '<span class="preCode-value">"$1"</span>') + '\n';
       }
     }
+    preCode.appendChild(preCodeInner);
     preHtml.appendChild(preCode);
     preHtml.appendChild(preLines);
     $('<span class="preCode-copy">copy</span>').appendTo(preHtml);
